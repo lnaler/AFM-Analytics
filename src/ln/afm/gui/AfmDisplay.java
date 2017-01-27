4093fce80980daccd2269e44eaf2e3d274aac4dc
Lynette Naler <lnaler@vt.edu>
2017-01-27 12:20:44 -0500

    Basic JavaFX embedding and prepping Crosshairs

--- src/ln/afm/gui/AfmDisplay.java
+++ src/ln/afm/gui/AfmDisplay.java
@@ -1,7 +1,7 @@
 package ln.afm.gui;
 
 import java.awt.BasicStroke;
-import java.awt.Color;
+//import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
@@ -18,16 +18,14 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
-import javax.swing.JSlider;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.border.BevelBorder;
-import javax.swing.border.EtchedBorder;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.fx.ChartViewer;
@@ -39,12 +37,22 @@ import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.general.DatasetUtilities;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.RectangleEdge;
 
+import javafx.application.Application;
 import javafx.application.Platform;
-import net.miginfocom.swing.MigLayout;
 import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import net.miginfocom.swing.MigLayout;
+import javafx.scene.paint.Color;
 
 public class AfmDisplay{
 	private static final Logger LOGGER = Logger.getLogger(AfmDisplay.class.getName() );
@@ -94,17 +102,22 @@ public class AfmDisplay{
 		frmAfmanalytics.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmAfmanalytics.getContentPane().setLayout(new MigLayout("", "[125.00px:100.00px:125.00px,grow][125.00px:100px][45.00px:120.00px][90.00:80.00][121.00px:87.00px:85.00px,grow][:100px:91.00px,grow][100.00px:61.00px,grow][100px,grow]", "[35,grow][35px,grow][35px][35px][35px][35px][35][][35.00][grow][][][grow]"));
 		
-		ChartPanel chartPanel = new ChartPanel((JFreeChart) null);
-		chartPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		chartPanel.setBackground(Color.WHITE);
-		frmAfmanalytics.getContentPane().add(chartPanel, "cell 0 1 5 7,grow");
+		JFXPanel fxPanel = new JFXPanel(); //https://docs.oracle.com/javase/8/javafx/interoperability-tutorial/swing-fx-interoperability.htm
+		frmAfmanalytics.getContentPane().add(fxPanel, "cell 0 1 5 7");
+		
+		Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                initFX(fxPanel);
+            }
+		});
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setViewportBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		frmAfmanalytics.getContentPane().add(scrollPane, "cell 5 1 3 7,grow");
 		
 		
-		
+		//test
 		JTextArea log = new JTextArea();
 		scrollPane.setViewportView(log);
 		
@@ -126,7 +139,7 @@ public class AfmDisplay{
 //				         true, 
 //				         false);
 				JFreeChart chart = data.getXYChart();
-				chartPanel.setChart(chart);
+				//chartPanel.setChart(chart);
 			}
 		});
 		frmAfmanalytics.getContentPane().add(btnRun, "cell 6 8");
@@ -238,4 +251,113 @@ public class AfmDisplay{
 	      dataset.setValue( "Nokia Lumia" , new Double( 10 ) );  
 	      return dataset;         
 	   }
+	
+	private static void initFX(JFXPanel fxPanel) {
+        // This method is invoked on the JavaFX thread
+        Scene scene = createScene();
+        fxPanel.setScene(scene);
+    }
+
+    private static Scene createScene() {
+        Group  root  =  new  Group();
+        Scene  scene  =  new  Scene(root, Color.ALICEBLUE);
+        Text  text  =  new  Text();
+        
+        text.setX(40);
+        text.setY(100);
+        text.setFont(new Font(25));
+        text.setText("Welcome JavaFX!");
+
+        root.getChildren().add(text);
+
+        return (scene);
+    }
+	  
+//    static class MyDemoPane extends StackPane implements ChartMouseListenerFX { //From JFreeChart CrosshairOverlayFXDemo1
+//        
+//        private ChartViewer chartViewer;
+//        private Crosshair xCrosshair;
+//        private Crosshair yCrosshair;
+//    
+//        public MyDemoPane() {
+//            XYDataset dataset = createDataset();
+//            JFreeChart chart = createChart(dataset); 
+//            this.chartViewer = new ChartViewer(chart);
+//            this.chartViewer.addChartMouseListener(this);
+//            getChildren().add(this.chartViewer);
+//           
+//            CrosshairOverlayFX crosshairOverlay = new CrosshairOverlayFX();
+//            this.xCrosshair = new Crosshair(Double.NaN, Color.GRAY, 
+//                    new BasicStroke(0f));
+//            this.xCrosshair.setStroke(new BasicStroke(1.5f, 
+//                    BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1, 
+//                    new float[]{2.0f, 2.0f}, 0));
+//            this.xCrosshair.setLabelVisible(true);
+//            this.yCrosshair = new Crosshair(Double.NaN, Color.GRAY, 
+//                    new BasicStroke(0f));
+//            this.yCrosshair.setStroke(new BasicStroke(1.5f, 
+//                    BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1, 
+//                    new float[] {2.0f, 2.0f}, 0));
+//            this.yCrosshair.setLabelVisible(true);
+//            crosshairOverlay.addDomainCrosshair(xCrosshair);
+//            crosshairOverlay.addRangeCrosshair(yCrosshair);
+//            
+//            Platform.runLater(() -> {
+//                this.chartViewer.getCanvas().addOverlay(crosshairOverlay);
+//            });
+//        }
+//
+//        @Override
+//        public void chartMouseClicked(ChartMouseEventFX event) {
+//            // ignore
+//        }
+//
+//        @Override
+//        public void chartMouseMoved(ChartMouseEventFX event) {
+//            Rectangle2D dataArea = this.chartViewer.getCanvas().getRenderingInfo().getPlotInfo().getDataArea();
+//            JFreeChart chart = event.getChart();
+//            XYPlot plot = (XYPlot) chart.getPlot();
+//            ValueAxis xAxis = plot.getDomainAxis();
+//            double x = xAxis.java2DToValue(event.getTrigger().getX(), dataArea, 
+//                    RectangleEdge.BOTTOM);
+//            // make the crosshairs disappear if the mouse is out of range
+//            if (!xAxis.getRange().contains(x)) { 
+//                x = Double.NaN;                  
+//            }
+//            double y = DatasetUtilities.findYValue(plot.getDataset(), 0, x);
+//            this.xCrosshair.setValue(x);
+//            this.yCrosshair.setValue(y);
+//        }
+//        
+//    }
+// 
+//    public static XYDataset createDataset() {
+//        XYSeries series = new XYSeries("S1");
+//        for (int x = 0; x < 10; x++) {
+//            series.add(x, x + Math.random() * 4.0);
+//        }
+//        XYSeriesCollection dataset = new XYSeriesCollection(series);
+//        return dataset;
+//    }
+//
+//    public static JFreeChart createChart(XYDataset dataset) {
+//        JFreeChart chart = ChartFactory.createXYLineChart(
+//                "CrosshairOverlayDemo1", "X", "Y", dataset);
+//        return chart;
+//    }
+//
+//    /**
+//     * Adds a chart viewer to the stage and displays it.
+//     * 
+//     * @param stage  the stage.
+//     * @throws Exception if something goes wrong.
+//     */
+//    public void start(Stage stage) throws Exception {
+//        stage.setScene(new Scene(new MyDemoPane())); 
+//        stage.setTitle("JFreeChart: CrosshairOverlayFXDemo1.java"); 
+//        stage.setWidth(700);
+//        stage.setHeight(390);
+//        stage.show(); 
+//    }
+    
 }
